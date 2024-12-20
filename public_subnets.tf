317077092d3acd4b6a9f1226d6ba0c05af3e4917
Tiago Teixeira Santos <tiagots86@hotmail.com>
2024-12-20 18:58:48 -0300

    rota de saída internet para subnets públicas

--- public_subnets.tf
+++ public_subnets.tf
@@ -14,3 +14,25 @@ resource "aws_subnet" "public" {
     aws_vpc_ipv4_cidr_block_association.main
   ]
 }
+
+resource "aws_route_table" "public_internet_access" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-public-access"
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public_internet_access.id
+  destination_cidr_block = "0.0.0.0/0"
+
+  gateway_id = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public_internet_access.id
+}
